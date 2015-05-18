e4f6a6de88f02b650e6a7388fc20a9f07cabc1ab
EddieTwo <grutmae@NYCW0366.nasa.westlb.net>
2015-05-18 16:08:41 -0400

    Update

--- Move.cpp
+++ Move.cpp
@@ -3,6 +3,43 @@
 
 using namespace std;
 
+class TestClass
+{
+	int _i;
+	string _str;
+public:
+	TestClass(int i, const string& str): _i(i), _str(str) {}
+	
+	TestClass(const TestClass& tc): _i(tc._i), _str(tc._str) 
+	{
+		cout << "copy constructor" << endl;
+	}
+
+	TestClass(TestClass&& tc): _i(tc._i), _str(move(tc._str)) 
+	{
+		cout << "move constructor" << endl;
+	}
+
+	TestClass& operator=(const TestClass& tc)
+	{
+		_i = tc._i;
+		_str = tc._str;
+		cout << "copy assignment operator" << endl;
+
+		return *this;
+	}
+
+	TestClass& operator=(TestClass&& tc)
+	{
+		_i = tc._i;
+		_str = move(tc._str);
+		cout << "move assignment operator" << endl;
+
+		return *this;
+	}
+
+};
+
 vector<int> doubleValues(const vector<int>& v)
 {
 	vector<int> vd;
@@ -14,8 +51,16 @@ vector<int> doubleValues(const vector<int>& v)
 
 int main()
 {
+	// 
 	vector<int> v = { 1, 4, 9, -2, -12, 11, };
 	vector<int> vd = doubleValues(v);
+
+	vd = v;
+	vd = ref(v);
+
+	// bespoke class
+	TestClass tc(1, "test");
+	TestClass tc2 = tc;
 
 	return 0;
 }
